3f954f21115ef547646b05a84837650370e41da4
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-10 18:59:35 +0700

    first commit

---
+++ SkinCareSellProductSysterm/src/main/java/com/example/SkinCareSellProductSysterm/SkinCareSellProductSystermApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkinCareSellProductSysterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinCareSellProductSystermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinCareSellProductSystermApplication.class, args);
+	}
+
+}
---
+++ SkinCareSellProductSysterm/src/test/java/com/example/SkinCareSellProductSysterm/SkinCareSellProductSystermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkinCareSellProductSysterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCareSellProductSystermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
