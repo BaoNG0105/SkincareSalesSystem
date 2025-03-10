074adcaf5b835515ee56626a66797290d74951d3
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-10 20:38:10 +0700

    Mã hóa mÃật khÃẩu ÃUser

---
+++ SkinCareSellProductSysterm/src/main/java/com/example/SkinCareSellProductSysterm/Configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.SkinCareSellProductSysterm.Configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// localhost:8080 => localhost:3000 || localhost:5173 vite
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // http://localhost:3000/
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+}
