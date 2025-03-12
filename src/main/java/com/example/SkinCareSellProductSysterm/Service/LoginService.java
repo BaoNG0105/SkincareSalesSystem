69003a79d10acbe488ff5b5a638eb128a73fb117
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-12 17:07:32 +0700

    Finished Login User

---
+++ src/main/java/com/example/SkinCareSellProductSysterm/Service/LoginService.java
@@ -0,0 +1,42 @@
+package com.example.SkinCareSellProductSysterm.Service;
+
+import com.example.SkinCareSellProductSysterm.DTO.LoginRequest;
+import com.example.SkinCareSellProductSysterm.Entity.User;
+import com.example.SkinCareSellProductSysterm.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+
+    public String authenticateUser(LoginRequest loginRequest) {
+        // Tìm người dùng theo email (chỉ những tài khoản chưa bị xóa)
+        User user = userRepository.findByEmailAndIsDeletedFalse(loginRequest.getEmail())
+                .orElseThrow(() -> new BadCredentialsException("Email hoặc mật khẩu không chính xác"));
+
+        // Kiểm tra mật khẩu
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPasswordHash())) {
+            throw new BadCredentialsException("Email hoặc mật khẩu không chính xác");
+        }
+
+        // Kiểm tra trạng thái tài khoản (active hay không)
+        if (!user.isStatus()) {
+            throw new DisabledException("Tài khoản của bạn đã bị vô hiệu hóa");
+        }
+
+        String token = jwtService.generateToken(user);
+        // Đăng nhập thành công, trả về id người dùng
+        return token;
+    }
+}
