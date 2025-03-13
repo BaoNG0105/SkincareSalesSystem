7f99f3a1c6dd882b38d7157eb36a93f59a09d797
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-13 20:49:21 +0700

    Request for RatingFeedback

--- src/main/java/com/example/SkinCareSellProductSysterm/DTO/RatingFeedbackRequest.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/DTO/RatingFeedbackRequest.java
@@ -1,4 +1,56 @@
 package com.example.SkinCareSellProductSysterm.DTO;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public class RatingFeedbackRequest {
+
+    @NotNull
+    private long userId;
+
+    @NotNull
+    private long productId;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "comment", columnDefinition = "TEXT")
+    private String comment;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
