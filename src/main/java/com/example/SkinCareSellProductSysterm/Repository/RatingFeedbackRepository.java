f8beed630929a06736b32bb083b7b97717d42e0c
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-13 20:52:54 +0700

    Service and Repository For RatingFeedback

--- src/main/java/com/example/SkinCareSellProductSysterm/Repository/RatingFeedbackRepository.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/Repository/RatingFeedbackRepository.java
@@ -1,4 +1,20 @@
 package com.example.SkinCareSellProductSysterm.Repository;
 
-public class RatingFeedbackRepository {
+import com.example.SkinCareSellProductSysterm.Entity.RatingFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RatingFeedbackRepository extends JpaRepository<RatingFeedback, Long> {
+
+    List<RatingFeedback> findAllByProduct_ProductIdAndIsDeletedFalse(long productId);
+
+
+    List<RatingFeedback> findByIsDeletedFalse();
+
+    Optional<RatingFeedback> findByIdAndIsDeletedFalse(long feedbackId);
 }
+
