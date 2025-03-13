7fd710ded378624e493700920986e2d6d3b52f50
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-13 20:36:03 +0700

    Controller and Service For Product

--- src/main/java/com/example/SkinCareSellProductSysterm/Controller/ProductController.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/Controller/ProductController.java
@@ -1,4 +1,45 @@
 package com.example.SkinCareSellProductSysterm.Controller;
 
+import com.example.SkinCareSellProductSysterm.DTO.ProductRequest;
+import com.example.SkinCareSellProductSysterm.Entity.Product;
+import com.example.SkinCareSellProductSysterm.Service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5173") // Thêm port của frontend vào đây
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductRequest productRequest){
+        return ResponseEntity.ok(productService.createProduct(productRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProduct(){
+        return ResponseEntity.ok(productService.getAllProduct());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable long id){
+        productService.delete(id);
+        return ResponseEntity.ok("Deleted product with ID: " + id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @Valid @RequestBody ProductRequest productRequest) {
+        return ResponseEntity.ok(productService.updateProduct(id, productRequest));
+    }
 }
