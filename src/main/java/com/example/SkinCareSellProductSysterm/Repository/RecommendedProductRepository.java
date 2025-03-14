b23ed190037c226cd85c559f8addf6c3f552e30a
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-14 23:24:57 +0700

    Repository RecommentProduct

--- src/main/java/com/example/SkinCareSellProductSysterm/Repository/RecommendedProductRepository.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/Repository/RecommendedProductRepository.java
@@ -1,4 +1,20 @@
 package com.example.SkinCareSellProductSysterm.Repository;
 
-public interface RecommendedProductRepository {
+import com.example.SkinCareSellProductSysterm.Entity.RecommendedProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RecommendedProductRepository extends JpaRepository<RecommendedProduct, Long> {
+
+    List<RecommendedProduct> findAllByProduct_ProductIdAndIsDeletedFalse(long product);
+    List<RecommendedProduct> findAllBySkinType_SkinTypeIdAndIsDeletedFalse(long skinType);
+
+    List<RecommendedProduct> findByIsDeletedFalse();
+
+    Optional<RecommendedProduct> findByRecommendationIdAndIsDeletedFalse(long answerId);
+
 }
