a59bb5ec6027be9ee17b9c1d7e12624c0a618341
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-15 08:56:29 +0700

    Repository Request, Repository

--- src/main/java/com/example/SkinCareSellProductSysterm/DTO/RecommendedProductRequest.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/DTO/RecommendedProductRequest.java
@@ -1,4 +1,40 @@
 package com.example.SkinCareSellProductSysterm.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class RecommendedProductRequest {
+
+    @NotNull(message = "Product ID cannot be null")
+    private Long productId;
+
+    @NotNull(message = "Skin Type ID cannot be null")
+    private Long skinTypeId;
+
+    @Size(max = 255, message = "Recommendation reason must be at most 255 characters")
+    private String recommendationReason;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getSkinTypeId() {
+        return skinTypeId;
+    }
+
+    public void setSkinTypeId(Long skinTypeId) {
+        this.skinTypeId = skinTypeId;
+    }
+
+    public String getRecommendationReason() {
+        return recommendationReason;
+    }
+
+    public void setRecommendationReason(String recommendationReason) {
+        this.recommendationReason = recommendationReason;
+    }
 }
