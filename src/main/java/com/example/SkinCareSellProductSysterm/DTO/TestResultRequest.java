e42a5cc3e3a27b106c77d76258187a36f070c746
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-16 19:26:54 +0700

    TestResult Request

--- src/main/java/com/example/SkinCareSellProductSysterm/DTO/TestResultRequest.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/DTO/TestResultRequest.java
@@ -1,4 +1,27 @@
 package com.example.SkinCareSellProductSysterm.DTO;
 
+import lombok.Data;
+
+@Data
 public class TestResultRequest {
+    private Long userId;
+    private Long testResultId;
+
+    // Getter and Setter
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getTestResultId() {
+        return testResultId;
+    }
+
+    public void setTestResultId(Long testResultId) {
+        this.testResultId = testResultId;
+    }
 }
