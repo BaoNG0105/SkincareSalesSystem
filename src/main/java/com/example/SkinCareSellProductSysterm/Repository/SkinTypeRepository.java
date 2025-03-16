390f7e559eb1e09e4489f264f95a495f810b8331
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-16 10:20:33 +0700

    Request and Repository For SkinType

--- src/main/java/com/example/SkinCareSellProductSysterm/Repository/SkinTypeRepository.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/Repository/SkinTypeRepository.java
@@ -1,4 +1,13 @@
 package com.example.SkinCareSellProductSysterm.Repository;
 
-public class SkinTypeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SkinTypeRepository extends JpaRepository<SkinType, Long> {
+    List<SkinType> findByIsDeletedFalse();
+    Optional<SkinType> findBySkinTypeIdAndIsDeletedFalse(long id);
 }
